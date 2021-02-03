b4b97bfe314692c4a0d94aadbbdd08608f4b0a5c
mishrasarthak08 <70634871+mishrasarthak08@users.noreply.github.com>
2021-02-03 10:35:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function preload(){
+    
+}
+
+function setup(){
+   createCanvas(400,700);
+    engine = Engine.create();
+	world = engine.world;
+}
+
+function draw(){
+    
+}   
+
